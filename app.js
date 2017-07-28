ce1a7584c50f84d4186f46f6e7c54066a3f1dbb8
jhoun <jayhoun@gmail.com>
2017-07-27 19:40:58 -0700

    implement mvp

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-$.ajax({
-  url: 'https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?',
-  dataType: "jsonp",
-  success:function(result){
-    console.log(result)
-  }
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var channel = "<ul>";
+var status = "<ul>";
+
+users.forEach(function(users){
+  $.ajax({
+    url: `https://wind-bow.gomix.me/twitch-api/streams/${users}?callback=?`,
+    dataType: "jsonp",
+    success:function(result){
+      console.log('result.stream: ', result.stream);
+      if (result.stream === null || result.stream === undefined){
+        status += `<li>Offline</li>`;
+      } else {
+        status += `<li>${result.stream.channel.status}</li>`;
+      }
+      channel += `<li>${users}</li>`;
+      $(".channel_data").html(channel + "</ul>");
+      $(".status_data").html(status + "</ul>");
+    }
+  })
 })

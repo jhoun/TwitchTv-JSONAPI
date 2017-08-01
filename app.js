159d767af66a764a6480bdce2340339576028d80
jhoun <jayhoun@gmail.com>
2017-08-01 11:22:26 -0700

    added undefined when user not available

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404" ];
 
 users.forEach(function(users){
   $.ajax({
     url: `https://wind-bow.gomix.me/twitch-api/streams/${users}?callback=?`,
     dataType: "jsonp",
     success:function(result){
+
       var status;
       if (result.stream === null || result.stream === undefined){
         status = `Offline`;
@@ -15,10 +16,13 @@ users.forEach(function(users){
         url: `https://wind-bow.gomix.me/twitch-api/channels/${users}?callback=?`,
         dataType: "jsonp",
         success:function(result){
-          var logo = result.logo !== null ? result.logo : 'http://i.imgur.com/bdrXyHv.png';
+          status = result.status !== 404 ? status : 'Account Closed';
+          var name = result.name !== undefined ? result.name : users;
+          var url = result.status !== 404 ? result.url : "#";
+          var logo = result.logo !== null && result.status !== 404 ? result.logo : 'http://i.imgur.com/bdrXyHv.png';
           var html = `<div class="channel_container">
                     <div class="inner image"><img src=${logo}></div>
-                    <div class="inner name"><a href='${result.url}'><div>${result.name}</div></a></div>
+                    <div class="inner name"><a href='${url}'><div>${name}</div></a></div>
                     <div class="inner status">${status}</div>
                     </div>`;
           $("#display").append(html);
